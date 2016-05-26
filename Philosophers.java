0254ee5ced2bb2a982a4ee9d2a6787d3a8397dcb
JAY <jaybawankar123@gmail.com>
2016-05-26 01:21:51 -0700

    Update Philosophers.java

--- Philosophers.java
+++ Philosophers.java
@@ -1,16 +1,10 @@
 
+/*
+ *PROBLEM STATEMENT: Implementation of Threading (Dyning Philosopher Problem) 
+ */
+
 import static java.lang.Thread.sleep;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author JAY
- */
 
 class Table
 {
